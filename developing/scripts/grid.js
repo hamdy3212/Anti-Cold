3785fe249485db997cdf55580a6e24b10b630031
hamdy <hamdyyousseif32@gmail.com>
2020-11-22 01:12:52 +0200

    add more values to gender

--- developing/scripts/grid.js
+++ developing/scripts/grid.js
@@ -139,11 +139,6 @@ var gridOptions = {
 
 };
 
-// // setup the grid after the page has finished loading
-// document.addEventListener('DOMContentLoaded', function () {
-    
-// });
-
 var gridDiv = document.querySelector('#myGrid');
 new agGrid.Grid(gridDiv, gridOptions);
 var ref = firebase.database().ref();
